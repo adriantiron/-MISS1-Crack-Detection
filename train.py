35f70517d2b3b3059f4df6b6eaf1b8ac7f93d46e
adriantiron <tda_mda@yahoo.com>
2020-01-04 15:12:38 +0200

    Refactoring train.py

--- train.py
+++ train.py
@@ -7,98 +7,111 @@ import os, cv2, random, numpy as np
 img_size = 128
 batch_size = 64
 
-# Model initialization
-model = Sequential()
 
-model.add(Conv2D(16, (3, 3), activation="relu", input_shape=(img_size, img_size, 1)))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(.3))
+def model_init():
+    model = Sequential()
 
-model.add(Conv2D(32, (3, 3), activation="relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(.3))
+    model.add(Conv2D(16, (3, 3), activation="relu", input_shape=(img_size, img_size, 1)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(.3))
 
-model.add(Conv2D(32, (3, 3), activation="relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(.3))
+    model.add(Conv2D(32, (3, 3), activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(.3))
 
-model.add(Flatten())
-model.add(Dense(258, activation="relu"))
+    model.add(Conv2D(32, (3, 3), activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(.3))
 
-model.add(Dense(1, activation="sigmoid"))
+    model.add(Flatten())
+    model.add(Dense(258, activation="relu"))
 
-model.summary()
-model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+    model.add(Dense(1, activation="sigmoid"))
 
-# Preparing the data
-train_dir = "Dataset\\Train"
-validation_dir = "Dataset\\Test"
-categories = ["Positive", "Negative"]
-images_array = []
-val_array = []
+    model.summary()
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    return model
 
-for category in categories:
-    path = os.path.join(train_dir, category)
-    val_path = os.path.join(validation_dir, category)
-    class_num = categories.index(category)
 
-    for img in os.listdir(path):
-        filename, extens = os.path.splitext(img)
-        if extens in ['.jpg', '.png']:
-            img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
-            new_array = cv2.resize(img_array, (img_size, img_size))
-            images_array.append([new_array, class_num])
+def data_prep():
+    train_dir = "Dataset\\Train"
+    validation_dir = "Dataset\\Test"
+    categories = ["Positive", "Negative"]
+    images_array = []
+    val_array = []
 
-    for img in os.listdir(val_path):
-        filename, extens = os.path.splitext(img)
-        if extens in ['.jpg', '.png']:
-            img_array = cv2.imread(os.path.join(val_path, img), cv2.IMREAD_GRAYSCALE)
-            new_array = cv2.resize(img_array, (img_size, img_size))
-            val_array.append([new_array, class_num])
+    for category in categories:
+        path = os.path.join(train_dir, category)
+        val_path = os.path.join(validation_dir, category)
+        class_num = categories.index(category)
 
-random.shuffle(images_array)
-random.shuffle(val_array)
-x_train = []
-y_train = []
-x_val = []
-y_val = []
+        for img in os.listdir(path):
+            filename, extension = os.path.splitext(img)
+            if extension in ['.jpg', '.png']:
+                img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+                new_array = cv2.resize(img_array, (img_size, img_size))
+                images_array.append([new_array, class_num])
 
-for image, label in images_array:
-    x_train.append(image)
-    y_train.append(label)
+        for img in os.listdir(val_path):
+            filename, extension = os.path.splitext(img)
+            if extension in ['.jpg', '.png']:
+                img_array = cv2.imread(os.path.join(val_path, img), cv2.IMREAD_GRAYSCALE)
+                new_array = cv2.resize(img_array, (img_size, img_size))
+                val_array.append([new_array, class_num])
 
-for image, label in val_array:
-    x_val.append(image)
-    y_val.append(label)
+    random.shuffle(images_array)
+    random.shuffle(val_array)
+    x_train = []
+    y_train = []
+    x_val = []
+    y_val = []
 
-x_train = np.array(x_train).reshape(len(x_train), img_size, img_size, 1)
-x_val = np.array(x_val).reshape(len(x_val), img_size, img_size, 1)
+    for image, label in images_array:
+        x_train.append(image)
+        y_train.append(label)
 
-# This will slightly warp and transform the images so we have more training data
-aug = ImageDataGenerator(
-    rescale=1./255,
-    rotation_range=30,
-    zoom_range=0.15,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.15,
-    horizontal_flip=True,
-    fill_mode="nearest")
-aug.fit(x_train)
-aug.fit(x_val)
+    for image, label in val_array:
+        x_val.append(image)
+        y_val.append(label)
 
-# This saves the best model after an epoch, so we don't lose progress if it crashes
-checkpoint = ModelCheckpoint("keras-crack.model.h5", monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
-train = model.fit(
-    aug.flow(x_train, y_train, batch_size=batch_size),
-    steps_per_epoch=len(x_train)/batch_size,
-    epochs=10,
-    verbose=1,
-    validation_data=aug.flow(x_val, y_val, batch_size=batch_size),
-    validation_steps=len(x_val)/batch_size,
-    callbacks=[checkpoint]
-)
+    x_train = np.array(x_train).reshape(len(x_train), img_size, img_size, 1)
+    x_val = np.array(x_val).reshape(len(x_val), img_size, img_size, 1)
 
-model.save(os.path.join(os.getcwd(), 'crack.model.h5'))
+    return x_train, y_train, x_val, y_val
+
+
+def train_nn(model, x_train, y_train, x_val, y_val):
+    # This will slightly warp and transform the images so we have more training data
+    aug = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=30,
+        zoom_range=0.15,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.15,
+        horizontal_flip=True,
+        fill_mode="nearest")
+    aug.fit(x_train)
+    aug.fit(x_val)
+
+    # This saves the best model after an epoch, so we don't lose progress if it crashes
+    checkpoint = ModelCheckpoint("keras-crack.model.h5", monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
+    train = model.fit(
+        aug.flow(x_train, y_train, batch_size=batch_size),
+        steps_per_epoch=len(x_train)/batch_size,
+        epochs=10,
+        verbose=1,
+        validation_data=aug.flow(x_val, y_val, batch_size=batch_size),
+        validation_steps=len(x_val)/batch_size,
+        callbacks=[checkpoint]
+    )
+
+    model.save(os.path.join(os.getcwd(), 'crack.model.h5'))
+
+
+if __name__ == '__main__':
+    xt, yt, xv, yv = data_prep()
+    new_model = model_init()
+    train_nn(new_model, xt, yt, xv, yv)
